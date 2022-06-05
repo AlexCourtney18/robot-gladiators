b0fd955728fefc9766df09669b7ae32b99ad49f9
Alex <lex.courtney18@gmail.com>
2022-06-05 16:27:39 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -54,7 +54,15 @@ var fight= function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+    window.alert("Welcome to Robot Glatiators! Round " + ( i + 1 ));
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
+    //debugger;
     fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
