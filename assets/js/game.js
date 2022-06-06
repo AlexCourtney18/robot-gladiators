06ec557d3d31b3dba2091b045f303dcedf2a18a8
Alex <lex.courtney18@gmail.com>
2022-06-06 18:36:40 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -52,6 +52,11 @@ var fight= function(enemyName) {
 }  
 };
 
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
     window.alert('Welcome to Robot Glatiators! Round ' + (i + 1));
@@ -64,4 +69,25 @@ for (var i = 0; i < enemyNames.length; i++) {
         window.alert('You have lost your robot in battle! Game Over!');
         break;
     }
-}
+    }
+    endGame();
+};
+
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiatiors! Come back soon!");
+    }
+};
+
+startGame();
